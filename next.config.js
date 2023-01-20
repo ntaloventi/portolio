aba1ac7cb180940ec4da8304541f27984a174ad4
Iqbal Rizky <iqbal@cashlez.com>
2023-01-21 05:02:34 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// const withPWA = require('next-pwa')
+
+// module.exports = withPWA({
+//     pwa: {
+//         disable: process.env.NODE_ENV === 'development',
+//         dest: 'public'
+//     },
+    // images: {
+    //     domains: ['media-exp1.licdn.com', 'www.nonceblox.com']
+    // }
+// })
+module.exports = {
+    images: {
+        domains: ['media-exp1.licdn.com', 'www.nonceblox.com', "lh6.googleusercontent.com"]
+    }
+}
